59ab71c44effcf79c01142f0b0aa548371047fb6
Alejandro Echavarria <mechavarria@inespre.gob.do>
2022-05-26 15:07:11 -0400

    mechavarria-2022-05-26-work

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom';
 
 export const Navbar = () => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light p-2">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light p-2 shadow-sm">
         <Link className="navbar-brand" to="/">F&R</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
